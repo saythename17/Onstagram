39b2c9a2b25726d055e6590d10ac0669018b0ff4
FANXY_ON <zion.h719@gmail.com>
2020-09-16 14:11:13 +0900

    Connect to Firebase

--- app/src/main/java/com/icandothisallday2020/onstagram/LoginActivity.kt
+++ app/src/main/java/com/icandothisallday2020/onstagram/LoginActivity.kt
@@ -1,12 +1,60 @@
 package com.icandothisallday2020.onstagram
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
+    var auth : FirebaseAuth? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        auth=FirebaseAuth.getInstance()
+        email_LoginBtn.setOnClickListener { signInAndroid() }
+    }
+
+
+
+    fun signInAndroid() {
+        auth?.createUserWithEmailAndPassword(et_email.text.toString(), et_password.text.toString())
+            ?.addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    //creating a user account
+
+                } else if (!(task.exception?.message.isNullOrEmpty())) {
+                    //Show the error message
+                    Toast.makeText(this, task.exception?.message, Toast.LENGTH_LONG).show()
+                } else {
+                    //Login if you have account
+
+                }
+            }
+    }
+
+
+
+
+    fun signinEmail() {
+        auth?.signInWithEmailAndPassword(et_email.text.toString(), et_password.text.toString())
+            ?.addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    //Login
+                } else {
+                    //Show the message Login fail-- password/email not mach
+                }
+            }
+    }
+
+
+    fun moveMainPage(user: FirebaseUser?) {
+        if (user != null) {
+            startActivity(Intent(this, MainActivity::class.java))
+        }
     }
 }
